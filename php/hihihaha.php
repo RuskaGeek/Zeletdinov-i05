5539955d7fd8c828bae52b19cd0e545b873f9ab7
Zeletdinov <ruslan@zel.com>
2022-10-31 03:25:54 +0000

    hihihaha.html

---
+++ php/hihihaha.php
@@ -0,0 +1,26 @@
+<?php
+    if (empty($_GET['v1']) or empty($_GET['v2']) or empty ($_GET['act'])) {
+        die ('А где данные?');
+    }
+    if (is_numeric($_GET['v1']) and is_numeric($_GET['v2'])){
+    $x = $_GET['v1'];
+    $y = $_GET['v2'];
+    $act = $_GET['act'];
+    if ($act == '+'){
+        $z = $x + $y;
+    }
+    else if ($act == '-'){
+        $z = $x - $y;
+    }
+    else if ($act == '*'){
+        $z = $x * $y;
+    }
+    else if ($act == '/'){
+        if ($y == 0) die('Ты нафига на ноль делишь, глупышок?');
+        $z = $x / $y;
+    }
+    echo $x.' '.$act.' '.$y.' = '.$z;}
+    else{
+        echo "Введите числа";
+    }
+?> 

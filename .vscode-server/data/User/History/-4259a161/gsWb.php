b4087abe0db808e359f2cb6e384313fc032fc439
Zeletdinov <ruslan@zel.com>
2022-11-03 07:03:36 +0000

    *.php

---
+++ .vscode-server/data/User/History/-4259a161/gsWb.php
@@ -0,0 +1,27 @@
+<?php
+	if (empty($_GET['v1']) or empty($_GET['v2']) or empty($_GET['act'])){
+        die('А где данные?')
+    }
+    
+    $x = $_GET['v1'];
+    $y = $_GET['v2'];
+    $act = $_GET['act']
+    if ($act=='+'){
+        $z=$x+$y;
+    }
+    else if ($act=='-'){
+        $z=$x-$y;
+    }
+    else if($act=='+'){
+        $z=$x*$y;
+    }
+    else if($act=='/'){
+        if($y==0) die('Нафиг на ноль делишь, глупышок?');
+        $z=$x/$y;
+    }
+
+
+
+
+    echo $x.' '.$act.' '.$y.' = '.$z;
+?>
